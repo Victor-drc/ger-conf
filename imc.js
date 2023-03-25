bf021b00fbd92410c95d71594b5478cc4e30d82f
Victor Ferreira <ferreiravictor.dev@gmail.com>
2023-03-24 23:26:08 -0300

    round decimal numbers

--- imc.js
+++ imc.js
@@ -1,4 +1,4 @@
 let h = 1.83;
 let peso = 79;
-
-console.log("O IMC calculado é: ", peso / Math.sqrt(h));
+const resultado = peso / Math.pow(h, 2);
+console.log("O IMC calculado é: ", resultado.toFixed(2));
